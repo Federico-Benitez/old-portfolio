63df17fbbb6826ab39afda0d750652b21dfff655
Federico-Benitez <fedebenitez1996@gmail.com>
2021-03-03 10:11:54 -0300

    Iconos en la parte de tareas agregado y scrollbar

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -2,6 +2,11 @@ import React from "react";
 import Title from "../Components/Title";
 import ImageSection from "../Components/ImageSection";
 import SkillSection from "../Components/SkillSection";
+import ServicesSection from "../Components/ServicesSection";
+import webDevelop from "../img/webDevelop.svg";
+import testing from "../img/testing.svg";
+import deploy from "../img/deploy.svg";
+import api from "../img/api.svg";
 
 function AboutPage() {
   return (
@@ -18,6 +23,29 @@ function AboutPage() {
         <SkillSection skill={"NodeJS"} progress={"60%"} width={"60%"} />
         <SkillSection skill={"MySQL"} progress={"75%"} width={"75%"} />
         <SkillSection skill={"MongoDB"} progress={"60%"} width={"60%"} />
+      </div>
+      <Title title={"Tareas"} span={"Tareas"} />
+      <div className="services-container">
+        <ServicesSection
+          image={webDevelop}
+          title={"Desarrollo Web"}
+          text={"Desarrollo de aplicaciones web responsive"}
+        />
+        <ServicesSection
+          image={api}
+          title={"Desarrollo Rest API"}
+          text={"Desarrollo de APIs de tipo REST"}
+        />
+        <ServicesSection
+          image={testing}
+          title={"Testing"}
+          text={"Testeo de aplicaciones web"}
+        />
+        <ServicesSection
+          image={deploy}
+          title={"Deploy"}
+          text={"Desarrollo de aplicaciones web responsive"}
+        />
       </div>
     </div>
   );
