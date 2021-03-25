ed0bef941ed53c20d28fe9071ea6fb6467aa5fb0
Federico-Benitez <fedebenitez1996@gmail.com>
2021-03-24 21:24:45 -0300

    cambios en la ruta

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import "./App.scss";
-import { Switch, Route } from "react-router-dom";
+import './App.scss';
+import { Switch, Route } from 'react-router-dom';
 
-import NavBar from "./Components/NavBar";
-import HomePage from "./Pages/HomePage";
-import AboutPage from "./Pages/AboutPage";
-import PortfoliosPage from "./Pages/PortfoliosPage";
-import ContactPage from "./Pages/ContactPage";
-import { useState } from "react";
+import NavBar from './Components/NavBar';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import PortfoliosPage from './Pages/PortfoliosPage';
+import ContactPage from './Pages/ContactPage';
+import { useState } from 'react';
 
 function App() {
   const [navToggle, setNavToggle] = useState(false);
   const navClick = () => {
     setNavToggle(!navToggle);
-  }
+  };
   return (
     <div className="App">
       <div className={`sidebar ${navToggle ? 'nav-toggle' : ''}`}>
@@ -26,16 +26,16 @@ function App() {
       <div className="main-content">
         <div className="content">
           <Switch>
-            <Route path="/" exact>
+            <Route path="portfolio/" exact>
               <HomePage />
             </Route>
-            <Route path="/about" exact>
+            <Route path="portfolio/about" exact>
               <AboutPage />
             </Route>
-            <Route path="/portfolios" exact>
+            <Route path="portfoliosportfolios" exact>
               <PortfoliosPage />
             </Route>
-            <Route path="/contact" exact>
+            <Route path="portfolio/contact" exact>
               <ContactPage />
             </Route>
           </Switch>
